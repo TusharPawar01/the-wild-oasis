1eb3a40a83efd0b24ccf9a218d5df9cd1796175a
TusharPawar01 <tusharpawar20049@gmail.com>
2024-02-10 13:55:21 +0530

    Updated DarkMode file

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -3,7 +3,8 @@ import Logout from "../features/authentication/Logout";
 import ButtonIcon from "./ButtonIcon";
 import { HiOutlineUser } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
-import DarkmodeToggle from "./DarkmodeToggle";
+// import DarkmodeToggle from "./DarkmodeToggle";
+import DarkmodeToggle from "../ui/DarkModeToggle"
 
 const StyledHeaderMenu = styled.ul`
   display: flex;
